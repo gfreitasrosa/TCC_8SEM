58d6df62577dec35117d03187ea9f3d375111cbb
SamuelQNunes <samuelqnunes@hotmail.com>
2025-05-11 23:47:12 -0300

    Update final do Front

--- login/home/static/js/script_home.js
+++ login/home/static/js/script_home.js
@@ -128,7 +128,8 @@ document.getElementById("save-trail").addEventListener("click", async function (
             console.error('Erro na requisição:', error);
         }
     } else {
-        alert("Por favor insira um nome e uma data final para a trilha.");
+        //alert("Por favor insira um nome e uma data final para a trilha.");
+        showFailedPopup("Por favor, insira um nome e uma data final para a trilha.");
     }
 });
 
@@ -223,7 +224,8 @@ function displayTrailScreen(name, date, reminder) {
                 document.getElementById("create-task-popup").style.display = "none";
                 document.getElementById("task-name").value = "";
             } else {
-                alert("Por favor, insira um nome para a task.");
+                //alert("Por favor, insira um nome para a task.");
+                showFailedPopup("Por favor, insira um nome para a tarefa.");
             }
         });
     }
@@ -704,11 +706,11 @@ profileForm.addEventListener('submit', async function (event) {
 
         const data = await response.json();
         if (data.success) {
-            alert('Perfil atualizado com sucesso!');
+            //alert('Perfil atualizado com sucesso!');
             showSuccessPopup(data.message);
             profilePopup.style.display = 'none'; // Fechar o popup
         } else {
-            alert('Erro ao atualizar o perfil: ' + data.message);
+            //alert('Erro ao atualizar o perfil: ' + data.message);
             showFailedPopup(data.message);
         }
     } catch (error) {
@@ -735,7 +737,8 @@ async function saveTrail() {
             });
             if (response.ok) {
                 const data = await response.json();
-                alert("Trilha já existente!");
+                //alert("Trilha já existente!");
+                showFailedPopup("Trilha já existente!");
             } else {
                 const response = await fetch('/api/trails/', {
                     method: 'POST',
@@ -761,7 +764,7 @@ async function saveTrail() {
                 } else {
                     const errorData = await response.json();
                     console.error("Error details:", errorData);
-                    alert("Error saving trail: " + JSON.stringify(errorData));
+                    //alert("Error saving trail: " + JSON.stringify(errorData));
                     showFailedPopup("Erro ao criar a trilha. Tente novamente.");
                 }
                     }
@@ -771,7 +774,8 @@ async function saveTrail() {
 
         
     } else {
-        alert("Por favor insira um nome e uma data final para a trilha.");
+        //alert("Por favor insira um nome e uma data final para a trilha.");
+        showFailedPopup("Por favor, insira um nome e uma data final para a trilha.");
     }
 }
 
@@ -806,7 +810,7 @@ async function saveTask(taskName, taskId) {
         // Atualize a interface aqui (exemplo: atualize os detalhes da tarefa)
 
     } else {
-        alert("Erro ao atualizar a tarefa.");
+        //alert("Erro ao atualizar a tarefa.");
         showFailedPopup("Erro ao atualizar a tarefa. Tente novamente.");
     }
 }
@@ -935,7 +939,8 @@ async function saveOrUpdateTask(taskName, taskId = null) {
             showSuccessPopup("Tarefa criada com sucesso");
 
         } else {
-            alert(`Erro ao criar a tarefa`);
+            //alert(`Erro ao criar a tarefa`);
+            showFailedPopup("Erro ao criar a tarefa. Tente novamente.");
         }
         }
 
@@ -952,7 +957,8 @@ async function loadTrails() {
         console.log("Trilhas carregadas:", trails);
         // Adicione cada trilha à interface aqui
     } else {
-        alert("Erro ao carregar trilhas.");
+        //alert("Erro ao carregar trilhas.");
+        showFailedPopup("Erro ao carregar trilhas. Tente novamente.");
     }
 }
 
